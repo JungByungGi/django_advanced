217b0a1ae18fdb49fb307effd7c332258a6ee142
JungByungGi <qudrlwjd12@naver.com>
2021-05-06 13:31:11 +0900

    django-admin order list 페이지 커스터 마이징(order 모델 상태 추가 및 리스트 페이지 커스터 마이징)

--- order/admin.py
+++ order/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from .models import Order
+from django.utils.html import format_html   # html escape를 안 통하게 함(html처럼 태그로 쓰겠다.)
 
 
-# Register your models here.
-
+# 관리자 페이지 커스터마이징
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('fcuser', 'product')
+    list_filter = ('status',)  # 필터 항목 활성화
+    list_display = ('fcuser', 'product', 'styled_status')
+
+    # 상태 필터 커스터마이징
+    # f는 문자열 formatting
+    def styled_status(self, obj):
+        if obj.status == '환불':
+            return format_html(f'<span style="color:red">{obj.status}</span>')
+        if obj.status == '결제완료':
+            return format_html(f'<span style="color:blue">{obj.status}</span>')
+
+        return obj.status
+    # 상태바 변경
+    styled_status.short_description = '상태'
 
 
 admin.site.register(Order, OrderAdmin)
--- order/models.py
+++ order/models.py
@@ -7,8 +7,20 @@ class Order(models.Model):
     fcuser = models.ForeignKey('fcuser.Fcuser', on_delete=models.CASCADE, verbose_name='사용자')
     product = models.ForeignKey('product.Product', on_delete=models.CASCADE, verbose_name='상품')
     quantity = models.IntegerField(verbose_name='수량') # integerfield : 정수 데이터 저장
+    # 기존 데이터들에 대한 memo, status 값은 어떻게 처리할 지 모르기 때문에 default, null과 blank를 넣어줌.
+    status = models.CharField(
+        #관리자 페이지에서 상태 선택 가능
+        choices=(
+            ('대기중', '대기중'),
+            ('결제대기', '결제대기'),
+            ('결제완료', '결제완료'),
+            ('환불', '환불'),
+        ),
+        default='대기중', max_length=32, verbose_name='상태')
+    memo = models.TextField(null=True, blank=True, verbose_name='메모')
     register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록 날짜') # auto_now_add : 최초 저장 시에만 현재날짜를 적용 / auto_now는 save 할 때마다 현재날짜로 갱신
-
+    
+    
     def __str__(self):
         return str(self.fcuser) + ' ' + str(self.product)
 
