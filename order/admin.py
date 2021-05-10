1730ec555a0ce345eacc9cbd10b551cd21ddd791
JungByungGi <qudrlwjd12@naver.com>
2021-05-10 18:00:39 +0900

    커스텀 액션 추가하기

--- order/admin.py
+++ order/admin.py
@@ -1,12 +1,27 @@
+from django.db.models import F
 from django.contrib import admin
 from .models import Order
-from django.utils.html import format_html   # html escape를 안 통하게 함(html처럼 태그로 쓰겠다.)
+from django.utils.html import format_html  # html escape를 안 통하게 함(html처럼 태그로 쓰겠다.)
+
+
+def refund(modeladmin, request, queryset):
+    queryset.update(status='환불')
+    for obj in queryset:
+        obj.product.stock += obj.quantity
+        obj.product.save()
+
+
+refund.short_description = '환불'
 
 
 # 관리자 페이지 커스터마이징
 class OrderAdmin(admin.ModelAdmin):
     list_filter = ('status',)  # 필터 항목 활성화
     list_display = ('fcuser', 'product', 'styled_status')
+
+    actions = [
+        refund
+    ]
 
     # 상태 필터 커스터마이징
     # f는 문자열 formatting
@@ -17,9 +32,9 @@ class OrderAdmin(admin.ModelAdmin):
             return format_html(f'<span style="color:blue">{obj.status}</span>')
 
         return obj.status
-    
+
     def changelist_view(self, request, extra_context=None):
-        extra_context = { 'title': '주문 목록'}
+        extra_context = {'title': '주문 목록'}
         return super().changelist_view(request, extra_context)
 
     def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
